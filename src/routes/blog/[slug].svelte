8692fa2d01ac4503be6dc78c6ab43ea7a40cc1c4
Marc <marc_lorrain@live.ca>
2020-05-20 11:39:04 -0400

    blog post layout

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -17,6 +17,9 @@
               createdAt
               title
               content
+              blogImg{
+                url
+              }
             }
           }`,
           variables: {
@@ -57,14 +60,13 @@
 	.content :global(pre) {
 		background-color: #f9f9f9;
 		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
-		padding: 0.5em;
+		/*padding: 0.5em;*/
 		border-radius: 2px;
-		overflow-x: auto;
+    overflow-x: auto;
 	}
 
 	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
+    background-color: transparent;
 	}
 
 	.content :global(ul) {
@@ -73,15 +75,34 @@
 
 	.content :global(li) {
 		margin: 0 0 0.5em 0;
-	}
+  }
+  .content p{
+    /*display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-content: space-around;*/
+  }
+  .container{
+    padding: 2rem;
+    width: 80%;
+  }
+
+img {
+    width: 25%;
+    height: auto;
+    margin: 2rem;
+}
 </style>
 
 <svelte:head>
 	<title>{post.title}</title>
 </svelte:head>
+<div class="container">
 
 <h1>{post.title}</h1>
-
 <div class='content'>
+  <img src={post.blogImg.url} alt="something" align="left"/>
 	{@html marked(post.content)}
+</div>
+
 </div>
